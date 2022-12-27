3ac2534f49bc4bb45cf3a4b5f2feedaaadde33b9
MALWICK <batahumphrey66@gmail.com>
2022-12-27 05:31:02 +0100

    creating the functionality of my progress bar

--- asset/script/progress.js
+++ asset/script/progress.js
@@ -6,32 +6,31 @@ const circles = document.querySelectorAll('.circle')
 let currentActive = 1
 
 next.addEventListener('click', () => {
-    currentActive++
+  currentActive++
 
-    if (currentActive > circles.length) {
-        currentActive = circles.length
-    }
+  if (currentActive > circles.length) {
+    currentActive = circles.length
+  }
 
-    update()
+  update()
 })
-
 
 prev.addEventListener('click', () => {
-    currentActive--
+  currentActive--
 
-    if (currentActive < 1) {
-        currentActive = 1
-    }
-    
-    update()
+  if (currentActive < 1) {
+    currentActive = 1
+  }
+
+  update()
 })
 
-function update() {
-    circles.forEach((circle, idx) => {
-        if (idx < currentActive) {
-            circle.classList.add('active')            
-        }else{
-             circle.classList.remove('active')
-        }
-    })
+function update () {
+  circles.forEach((circle, idx) => {
+    if (idx < currentActive) {
+      circle.classList.add('active')
+    } else {
+      circle.classList.remove('active')
+    }
+  })
 }
